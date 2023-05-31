84994ec674e016746e40330b0d385435175c8819
GerardoITA <gerardo.giusti@tiscali.it>
2023-05-31 14:32:58 +0200

    added classes

---
+++ src/org/lessons/java/Biglietto.java
@@ -0,0 +1,15 @@
+package org.lessons.java;
+
+public class Biglietto {
+    /*
+    Consegna: Il programma dovrà chiedere all’utente il numero (intero) di chilometri che vuole percorrere e l’età del passeggero (numero intero). Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+    il prezzo del biglietto è definito in base ai km (0.21 € al km)
+    va applicato uno sconto del 20% per i minorenni
+    va applicato uno sconto del 40% per gli over 65
+    Per chiedere informazioni all’utente sarà necessario usare la classe java.util.Scanner, come visto in classe stamattina.
+    Bonus: formattare il risultato con un numero limitato di decimali utilizzando la classe DecimalFormat
+    */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/org/lessons/java/FizzBuzz.java
@@ -0,0 +1,17 @@
+package org.lessons.java;
+
+public class FizzBuzz {
+    /*
+    Consegna: Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+    Esempio: 1 2 Fizz 4 Buzz Fizz 7 8 Fizz……
+    Bonus: invece che 100, chiedere all’utente quanti numeri vuole stampare (il valore inserito dall’utente deve essere maggiore di 0 e minore di 1000)
+    */
+    public static void main(String[] args) {
+
+        for ( int i = 1; i < 100; i++ ) {
+
+            System.out.println("test");
+
+        }
+    }
+}
