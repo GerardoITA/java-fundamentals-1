90699db7d8179077afcb64eac5a0ffc7f13ada62
GerardoITA <gerardo.giusti@tiscali.it>
2023-05-31 15:32:27 +0200

    fizzbuzz completed

--- src/org/lessons/java/Biglietto.java
+++ src/org/lessons/java/Biglietto.java
@@ -1,5 +1,7 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+import java.text.DecimalFormat;
 public class Biglietto {
     /*
     Consegna: Il programma dovrà chiedere all’utente il numero (intero) di chilometri che vuole percorrere e l’età del passeggero (numero intero). Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
@@ -11,5 +13,33 @@ public class Biglietto {
     */
     public static void main(String[] args) {
 
+        Scanner km = new Scanner(System.in);
+        System.out.println("Inserisci i km da percorrere");
+
+        int kmUtente = km.nextInt();
+
+        Scanner eta = new Scanner(System.in);
+        System.out.println("Inserisci la tua età");
+
+        int etaUtente = eta.nextInt();
+        double prezzoKm = 0.21;
+        double prezzoBase = ( kmUtente * prezzoKm );
+
+        double sconto = 0;
+        if ( etaUtente < 18) {
+            sconto = 0.20;
+        }
+        if ( etaUtente > 65 ) {
+            sconto = 0.40;
+        }
+
+        double prezzoFinale;
+
+        prezzoFinale = prezzoBase * ( 1 - sconto);
+
+        DecimalFormat dueDecimali;
+        dueDecimali = new DecimalFormat("0.00");
+
+        System.out.println("Il prezzo del biglietto è " + dueDecimali.format(prezzoFinale) + "€");
     }
 }
